b066592df8f67929f7410fe4a3c6f1600911240d
TY <185649066@qq.com>
2026-08-15 12:32:16 +0800

    HikingAlone Init

---
+++ src/main/java/com/ty/hikingalone/HikingAloneApplication.java
@@ -0,0 +1,13 @@
+package com.ty.hikingalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikingAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikingAloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ty/hikingalone/HikingAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.hikingalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingAloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
